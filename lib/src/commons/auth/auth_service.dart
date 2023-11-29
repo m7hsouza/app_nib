4f11a33ee4aa18ff758dfa74d04005e2ad0d378a
Matheus Lucas M Souza <m7hsouza@gmail.com>
2023-11-29 08:30:20 -0400

    bug fix

--- lib/src/commons/auth/auth_service.dart
+++ lib/src/commons/auth/auth_service.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:app_nib/src/commons/auth/credentials.dart';
 import 'package:app_nib/src/shared/services/http_service.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,12 +16,8 @@ class AuthService extends ChangeNotifier {
     final response = await client.post('/login', data: {
       'enrollment_number': credentials.enrollmentNumber,
       'password': credentials.password,
-    }).catchError(_onError);
+    });
 
     debugPrint(response.data['access_token']);
-  }
-
-  void _onError(Object error) {
-    debugPrint(error.toString());
   }
 }
